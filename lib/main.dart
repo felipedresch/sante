2b14490eb67212aade73432e753420606d9dc414
Felipe Dresch <felipe10dresch12@gmail.com>
2023-09-08 17:10:35 -0300

    Adição do multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -21,31 +21,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => TrackScreens(),
-      child: ChangeNotifierProvider(
-        create: (context) => ClienteRepository(),
-        child: ChangeNotifierProvider(
-          create: (context) => ConsultaRepository(),
-          child: GestureDetector(
-            onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-            child: MaterialApp(
-              title: 'Flutter Demo',
-              theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-                useMaterial3: true,
-              ),
-              home: const Home(),
-              routes: {
-                "/cadastrar": (_) => const Cadastro(),
-                "/list": (_) => const ClienteList(),
-                "/home": (_) => const Home(),
-                "/view": (_) => const CadastroView(),
-                "/consulta": (_) => const Consulta(),
-                "/consultaView": (_) => const ConsultaView(),
-              },
-            ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TrackScreens()),
+        ChangeNotifierProvider(create: (context) => ClienteRepository()),
+        ChangeNotifierProvider(create: (context) => ConsultaRepository()),
+      ],
+      child: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
           ),
+          home: const Home(),
+          routes: {
+            "/cadastrar": (_) => const Cadastro(),
+            "/list": (_) => const ClienteList(),
+            "/home": (_) => const Home(),
+            "/view": (_) => const CadastroView(),
+            "/consulta": (_) => const Consulta(),
+            "/consultaView": (_) => const ConsultaView(),
+          },
         ),
       ),
     );
